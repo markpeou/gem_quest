ba4e33ce3097b811448ea44aa5cbb27f0045fd56
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-02-06 17:37:37 +1100

    fixed routes.rb conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
-Rails.application.routes.draw do
 
   root to: '/', to: 'pages#home'
 
   resources :locations, only: [:index, :show]
   resources :quests, only: [:show]
+  resources :team_quests
+  resources :quests
+  resources :locations
+  resources :teams
+
+  get '/login', to: 'sessions#new'
+  post '/session', to: 'sessions#create'
+
 end
