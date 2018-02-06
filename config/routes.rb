21f700ccabbb895779bd618035dcc016e8d96028
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-02-06 17:15:29 +1100

    create login/session

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :quests
   resources :locations
   resources :teams
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+  post '/session', to: 'sessions#create'
+
 end
