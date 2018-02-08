32efdf7fd85815dc1752a0e7183b59a242caa97e
markpeou <markpeou@gmail.com>
2018-02-09 10:48:04 +1100

    updating finished app

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   post '/quests/:id', to: 'quests#check_answer'
-  # resources :team_quests
+  #resources :team_quests
   resources :teams
 
   resources :locations, only: [:index, :show]
