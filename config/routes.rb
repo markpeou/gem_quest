ad853810865555e9883de362e1be6c4694c2f8c1
markpeou <markpeou@gmail.com>
2018-03-14 16:00:28 +1100

    cleaned up some css

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   post '/quests/:id', to: 'quests#check_answer'
+
+  get '/quests/new', to: 'admins#new'
+  post '/quests', to: 'admins#create'
+
+  post '/quests/:id/edit', to: 'admins#update'
+
   # resources :team_quests
   resources :teams
 
